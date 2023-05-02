52c1c9f46f4f2f463981b6d153cf37d501f2f949
MGSimard <126427506+MGSimard@users.noreply.github.com>
2023-05-02 15:08:04 -0400

    Update jsonHandler.js

--- frontendmentor/results-summary-component/jsonHandler.js
+++ frontendmentor/results-summary-component/jsonHandler.js
@@ -2,6 +2,7 @@ const scoreReaction = document.getElementById("fetch-reaction");
 const scoreMemory = document.getElementById("fetch-memory");
 const scoreVerbal = document.getElementById("fetch-verbal");
 const scoreVisual = document.getElementById("fetch-visual");
+const scoreAvg = document.getElementById("user-score");
 
 loadResults();
 
@@ -9,9 +10,11 @@ function loadResults() {
     fetch("./data.json")
     .then(response => response.json())
     .then(data => {
+        const average = Math.floor((data[0].score + data[1].score + data[2].score + data[3].score)/4)
+        scoreAvg.innerHTML = average;
         scoreReaction.innerHTML = data[0].score;
         scoreMemory.innerHTML = data[1].score;
         scoreVerbal.innerHTML = data[2].score;
-        scoreVisual.innerHTML = data[3].score;
+        scoreVisual.innerHTML = data[3].score;        
     })
 }
